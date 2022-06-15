c419e671d602862c233a676e0d1f3e5b6fe3cd64
Mike Hukiewitz <m.h.hukiewitz@students.uu.nl>
2022-06-15 15:37:29 +0200

    fix render-layouts.ts

--- src/render-layouts.ts
+++ src/render-layouts.ts
@@ -10,25 +10,14 @@ export function renderLayoutsFiles(instructionsView: ViewInstructions | undefine
     let ixLayouts: string =
 `
 import { InstructionType } from '../types.js'
-export { IX_METHOD_CODE, IX_DATA_LAYOUT, IX_ACCOUNTS_LAYOUT } from '../../../ts/instructions.js'
+export { IX_DATA_LAYOUT, IX_ACCOUNTS_LAYOUT } from '../../../ts/instructions.js'
 
 export function getInstructionType(data: Buffer): InstructionType | undefined {
-    const method = data.slice(0, 1).readUInt8()
-    let bufferHumanReadable=[];
-    //Extract data from buffer into humanreadable instructiondecriminator
-    for (let i = 0; i < 8; i++) {
-      bufferHumanReadable.push(data.slice(i, i+1).readUInt8().toString())
-    }
-    let concatenatedInstruCode = bufferHumanReadable.toString()
-  
-    //const buffer = new Buffer([123, ..., 69])
-    //buffer.encode('ascii')
-  
-    return ixMap.get(concatenatedInstruCode)
-   // return IX_METHOD_CODE[method]
+  const discriminator = new Buffer(data.slice(0, 8).readUInt8())
+  return IX_METHOD_CODE.get(discriminator.toString('ascii'))
 }
 
-export const ixMap = new Map<string, InstructionType | undefined >([
+export const IX_METHOD_CODE = new Map<string, InstructionType | undefined >([
 `
     if(instructionsView != undefined) {
         for(let i = 0; i < instructionsView.instructions.length; i++){
