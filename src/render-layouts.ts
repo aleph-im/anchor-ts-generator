31d124dddda9be6a5471781ccc7314ac688fb6e8
ricardocr987 <32191898+ricardocr987@users.noreply.github.com>
2022-06-16 12:18:55 +0200

    layouts clean af

--- src/render-layouts.ts
+++ src/render-layouts.ts
@@ -1,11 +1,21 @@
 import { ViewInstructions, ViewAccounts } from './types'
-import { sha256 } from 'js-sha256'
-import { snakeCase } from 'snake-case'
-import camelcase from 'camelcase'
 
 export function renderLayoutsFiles(instructionsView: ViewInstructions | undefined, accountsView: ViewAccounts | undefined){
+
     let accountLayouts: string = 
-`export { AccountType, ACCOUNTS_DATA_LAYOUT } from '../../../ts/accounts.js'
+`import { 
+` 
+    if(accountsView != undefined) {
+      for(let i = 0; i < accountsView.accounts.length; i++){
+        accountLayouts += 
+`  ${accountsView.accounts[i].name.charAt(0).toLowerCase().concat(accountsView.accounts[i].name.slice(1))}Discriminator,
+`
+      }  
+
+    }
+    accountLayouts +=
+`} from '../../../ts-solita'
+export { AccountType, ACCOUNTS_DATA_LAYOUT } from '../../../ts/accounts.js'
 import { AccountType } from '../types.js'
 
 export const ACCOUNT_METHOD_CODE: Map<string, AccountType | undefined > = 
@@ -13,17 +23,26 @@ export const ACCOUNT_METHOD_CODE: Map<string, AccountType | undefined > =
 `
     if(accountsView != undefined) {
         for(let i = 0; i < accountsView.accounts.length; i++){
-            const discriminator = Array.from(instructionDiscriminator(accountsView.accounts[i].name))
             accountLayouts += 
-`     [Buffer.from([${discriminator}]).toString('ascii'), AccountType.${accountsView.accounts[i].name}],
+`   [Buffer.from(${accountsView.accounts[i].name.charAt(0).toLowerCase().concat(accountsView.accounts[i].name.slice(1))}Discriminator).toString('ascii'), AccountType.${accountsView.accounts[i].name}],
 `
         }
     }
     accountLayouts += 
 `])`
-  
+
     let ixLayouts: string =
+`import { 
+` 
+    if(instructionsView != undefined) {
+      for(let i = 0; i < instructionsView.instructions.length; i++){
+          ixLayouts += 
+`  ${instructionsView.instructions[i].name.charAt(0).toLowerCase().concat(instructionsView.instructions[i].name.slice(1))}InstructionDiscriminator,
 `
+      }
+    }
+    ixLayouts +=
+`} from '../../../ts-solita'
 import { InstructionType } from '../types.js'
 export { IX_DATA_LAYOUT, IX_ACCOUNTS_LAYOUT } from '../../../ts/instructions.js'
 
@@ -37,9 +56,8 @@ export const IX_METHOD_CODE: Map<string, InstructionType | undefined > =
 `
     if(instructionsView != undefined) {
         for(let i = 0; i < instructionsView.instructions.length; i++){
-            const ixDiscriminator = Array.from(instructionDiscriminator(instructionsView.instructions[i].name))
             ixLayouts += 
-`     [Buffer.from([${ixDiscriminator}]).toString('ascii'), InstructionType.${instructionsView.instructions[i].name}],
+`   [Buffer.from(${instructionsView.instructions[i].name.charAt(0).toLowerCase().concat(instructionsView.instructions[i].name.slice(1))}InstructionDiscriminator).toString('ascii'), InstructionType.${instructionsView.instructions[i].name}],
 `
         }
     }
@@ -48,24 +66,4 @@ export const IX_METHOD_CODE: Map<string, InstructionType | undefined > =
 `;
 
     return { accountLayouts, ixLayouts }
-  }
-
-  const SIGHASH_GLOBAL_NAMESPACE = 'global'
-
-  function instructionDiscriminator(name: string): Buffer {
-    return sighash(SIGHASH_GLOBAL_NAMESPACE, name)
-  }
-
-  function sighash(nameSpace: string, ixName: string): Buffer {
-    let name = snakeCase(ixName)
-    let preimage = `${nameSpace}:${name}`
-    return Buffer.from(sha256.digest(preimage)).slice(0, 8)
-  }
-
-  export const ACCOUNT_DISCRIMINATOR_SIZE = 8
-
-  export function accountDiscriminator(name: string): Buffer {
-    return Buffer.from(
-      sha256.digest(`account:${camelcase(name, { pascalCase: true })}`)
-    ).slice(0, ACCOUNT_DISCRIMINATOR_SIZE)
   }
